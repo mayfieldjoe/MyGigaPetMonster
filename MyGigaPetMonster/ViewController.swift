990f84c505bf8244d56b3c14246b21fea9f60933
mayfieldjoe <joemayfield81@gmail.com>
2016-03-29 13:20:47 -0400

    MyGigaMonster Learning Project
    Working with size classes and CGPoints.

--- MyGigaPetMonster/ViewController.swift
+++ MyGigaPetMonster/ViewController.swift
@@ -9,15 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+        
+    @IBOutlet weak var monsterImg: UIImageView!
+    @IBOutlet weak var foodImg: DragImg!
+    @IBOutlet weak var heartImg: DragImg!
+    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var imgArray = [UIImage]()
+        for var x = 1; x <= 4; x++ {
+            let img = UIImage(named: "idle\(x).png")
+            
+            imgArray.append(img!)
+        }
+        
+        monsterImg.animationImages = imgArray
+        monsterImg.animationDuration = 0.8
+        monsterImg.animationRepeatCount = 0
+        monsterImg.startAnimating()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        //print("i touched screen")
     }
 
 
