bdd71919e2ceb7151bf9b7757af52c3db18ce84d
mayfieldjoe <joemayfield81@gmail.com>
2016-03-31 13:26:21 -0400

    MonstserImg Class
    Added MonsterImg class to handle monster animation. Dragging and
    dropping food and hearts work in portrait
    mode.

--- MyGigaPetMonster/ViewController.swift
+++ MyGigaPetMonster/ViewController.swift
@@ -11,16 +11,19 @@ import UIKit
 class ViewController: UIViewController {
     
         
-    @IBOutlet weak var monsterImg: UIImageView!
+    @IBOutlet weak var monsterImg: MonsterImg!
     @IBOutlet weak var foodImg: DragImg!
     @IBOutlet weak var heartImg: DragImg!
+    
+    
+    
     
  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        var imgArray = [UIImage]()
+        /*var imgArray = [UIImage]()
         for var x = 1; x <= 4; x++ {
             let img = UIImage(named: "idle\(x).png")
             
@@ -30,7 +33,19 @@ class ViewController: UIViewController {
         monsterImg.animationImages = imgArray
         monsterImg.animationDuration = 0.8
         monsterImg.animationRepeatCount = 0
-        monsterImg.startAnimating()
+        monsterImg.startAnimating()*/
+        
+        foodImg.dropTarget = monsterImg
+        heartImg.dropTarget = monsterImg
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "itemDroppedOnCharacter:", name: "onTargetDropped", object: nil)
+        
+        
+    }
+    
+    func itemDroppedOnCharacter(notif: AnyObject)
+    {
+        print("Item dropeed on monster")
     }
 
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
