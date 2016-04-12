940f34fc7ea7d1df1060521466c129a411340d0f
mayfieldjoe <joemayfield81@gmail.com>
2016-04-12 11:42:45 -0400

    Timer added with death
    Timer and death animation is working.

--- MyGigaPetMonster/ViewController.swift
+++ MyGigaPetMonster/ViewController.swift
@@ -18,6 +18,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var penalty2Img: UIImageView!
     @IBOutlet weak var penalty3Img: UIImageView!
     
+    let DIM_ALPHA: CGFloat = 0.2
+    let OPAQUE: CGFloat = 1.0
+    let MAX_PENALTIES = 3
+    
+    var penalties = 0
+    var timer: NSTimer!
  
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,12 +41,6 @@ class ViewController: UIViewController {
         monsterImg.animationRepeatCount = 0
         monsterImg.startAnimating()*/
         
-        let DIM_ALPHA: CGFloat = 0.2
-        let OPAQUE: CGFloat = 1.0
-        let MAX_PENALTIES = 3
-        
-        var penalties = 0
-        var timer: NSTimer!
         
         
         penalty1Img.alpha = DIM_ALPHA
@@ -53,6 +53,7 @@ class ViewController: UIViewController {
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "itemDroppedOnCharacter:", name: "onTargetDropped", object: nil)
         
+        startTimer()
         
     }
     
@@ -63,8 +64,48 @@ class ViewController: UIViewController {
     
     func startTimer()
     {
+        if timer != nil {
+            timer.invalidate()
+        }
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "changeGameState", userInfo: nil, repeats: true)
 
     }
+    func changeGameState()
+    {
+        penalties++
+        
+        if penalties == 1
+        {
+            penalty1Img.alpha = OPAQUE
+            penalty2Img.alpha = DIM_ALPHA
+        }
+        else if penalties == 2
+        {
+            penalty2Img.alpha = OPAQUE
+            penalty3Img.alpha = DIM_ALPHA
+        }
+        else if penalties >= 3
+        {
+            penalty3Img.alpha = OPAQUE
+        }
+        else
+        {
+            penalty1Img.alpha = DIM_ALPHA
+            penalty2Img.alpha = DIM_ALPHA
+            penalty3Img.alpha = DIM_ALPHA
+        }
+        if penalties >= MAX_PENALTIES
+        {
+            gameOver()
+        }
+    }
+    func gameOver(){
+        timer.invalidate()
+        monsterImg.playDeathAnimation()
+    }
+    
+    
 
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         //print("i touched screen")
